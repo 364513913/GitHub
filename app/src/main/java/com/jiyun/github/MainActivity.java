d4129f84511a54c956ca5e657a6e6f72d31e7b96
lenovo <364513913@qq.com>
2017-09-12 10:33:48 +0800

    小曹

--- app/src/main/java/com/jiyun/github/MainActivity.java
+++ app/src/main/java/com/jiyun/github/MainActivity.java
@@ -1,13 +1,60 @@
 package com.jiyun.github;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+    private EditText editText2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                submit();
+
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String editTextString = editText.getText().toString().trim();
+        if (TextUtils.isEmpty(editTextString)) {
+            Toast.makeText(this, "editTextString不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String editText2String = editText2.getText().toString().trim();
+        if (TextUtils.isEmpty(editText2String)) {
+            Toast.makeText(this, "editText2String不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+
+
     }
 }
